d50fb19edfd7d95d78b136a4dfda87a23cdff5f3
James Holloway <james.holloway94@gmail.com>
2026-04-28 15:14:56 +0000

    bump postgres schema sync stamp after migration

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-04-28 15:06:51 UTC
+# Last sync: 2026-04-28 15:14:56 UTC
