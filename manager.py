ee4b481319b15b21549ff219decbbac6f6d2bd2e
James Holloway <james.holloway94@gmail.com>
2026-04-29 06:03:40 +0000

    validate bastion access time in security config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-04-29 05:00:21 UTC
+# Last sync: 2026-04-29 06:03:40 UTC
