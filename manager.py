94e5fee56464bc4a51048eefc8dd9c76eb394ce6
James Holloway <james.holloway94@gmail.com>
2026-04-29 16:43:07 +0000

    save postgres pool sync marker after refresh

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-04-29 15:13:23 UTC
+# Last sync: 2026-04-29 16:43:07 UTC
