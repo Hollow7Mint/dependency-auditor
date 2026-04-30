223bc0bb47fa176a2c06d1b708d183f912e9d0ef
James Holloway <james.holloway94@gmail.com>
2026-04-30 11:36:15 +0000

    log shield ddos event time in protection banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-04-30 11:00:03 UTC
+# Last sync: 2026-04-30 11:36:15 UTC
