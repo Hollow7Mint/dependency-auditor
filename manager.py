c6c6589d4f257360cee716b564308b7825314a7e
James Holloway <james.holloway94@gmail.com>
2026-05-03 11:55:04 +0000

    anchor ssh client config sync after key rotation

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-03 10:08:08 UTC
+# Last sync: 2026-05-03 11:55:04 UTC
