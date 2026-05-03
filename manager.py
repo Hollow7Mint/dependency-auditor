bf76b11da03cf18a6e2074f7a18e40c6eed1ef5f
James Holloway <james.holloway94@gmail.com>
2026-05-03 21:41:33 +0000

    trace ssh identity file sync for key audit

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-03 20:51:58 UTC
+# Last sync: 2026-05-03 21:41:33 UTC
