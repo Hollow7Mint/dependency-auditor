f70af10aadf1cfeef8ea342ab745829908888d13
James Holloway <james.holloway94@gmail.com>
2026-05-04 19:39:23 +0000

    merge mongodb shard sync time into metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-04 18:13:07 UTC
+# Last sync: 2026-05-04 19:39:22 UTC
