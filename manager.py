e5c78349712570b029d85aa4a44cf46ea27025ac
James Holloway <james.holloway94@gmail.com>
2026-05-05 09:28:18 +0000

    carry mongodb oplog sync stamp to config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-05 08:16:49 UTC
+# Last sync: 2026-05-05 09:28:18 UTC
