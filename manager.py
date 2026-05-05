8c31a73b78429c4a59b9892b3bc08a27947a1df0
James Holloway <james.holloway94@gmail.com>
2026-05-05 05:49:10 +0000

    set ssh tunnel sync stamp in config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-05 04:21:51 UTC
+# Last sync: 2026-05-05 05:49:09 UTC
