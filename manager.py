8ad6c6fb49119e02a36a95fb6fe46c0a610fa067
James Holloway <james.holloway94@gmail.com>
2026-05-05 14:30:06 +0000

    pin route53 dns sync stamp after record update

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-05 14:02:16 UTC
+# Last sync: 2026-05-05 14:30:06 UTC
