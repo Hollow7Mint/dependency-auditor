8867a1102456c8c53b7c5a188ab5698aeb49a403
James Holloway <james.holloway94@gmail.com>
2026-05-06 21:26:35 +0000

    validate s3 lifecycle rule time in deploy config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-06 20:36:51 UTC
+# Last sync: 2026-05-06 21:26:35 UTC
