2459507a233eea40f17ee190939f45110ae8c6a2
James Holloway <james.holloway94@gmail.com>
2026-05-10 02:02:40 +0000

    push secrets manager version stamp to metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-10 00:37:22 UTC
+# Last sync: 2026-05-10 02:02:39 UTC
