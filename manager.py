4c3b4a771404a73b2d0f10a1b9a7c401ca27b023
James Holloway <james.holloway94@gmail.com>
2026-05-10 14:50:18 +0000

    persist vpc route table sync after update

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-10 14:03:16 UTC
+# Last sync: 2026-05-10 14:50:18 UTC
