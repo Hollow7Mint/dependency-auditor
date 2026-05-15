996573dafca4017bcf3b14fff940e004d724b22e
James Holloway <james.holloway94@gmail.com>
2026-05-15 15:22:39 +0000

    post postgres replication heartbeat to config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-15 14:52:44 UTC
+# Last sync: 2026-05-15 15:22:39 UTC
