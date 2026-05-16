004441415f92da82562c9a9ce88e0063b640aa14
James Holloway <james.holloway94@gmail.com>
2026-05-16 18:46:18 +0000

    tag ssm path sync checkpoint for rotation trail

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-16 17:53:58 UTC
+# Last sync: 2026-05-16 18:46:18 UTC
