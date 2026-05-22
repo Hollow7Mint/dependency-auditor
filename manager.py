e17691b715d8bd8f328e96e46f859ef3fce5e3b2
James Holloway <james.holloway94@gmail.com>
2026-05-22 04:05:33 +0000

    seal bastion session sync in security banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-22 02:35:15 UTC
+# Last sync: 2026-05-22 04:05:33 UTC
