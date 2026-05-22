e2bf1958cad4493333788efc7198ae68795b6131
James Holloway <james.holloway94@gmail.com>
2026-05-22 00:54:21 +0000

    normalize mysql charset sync time in metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-21 23:30:52 UTC
+# Last sync: 2026-05-22 00:54:20 UTC
