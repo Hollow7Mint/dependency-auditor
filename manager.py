5627de3096a5f13008dd586e1b4d65917413ee64
James Holloway <james.holloway94@gmail.com>
2026-05-24 09:47:35 +0000

    backfill s3 versioning sync marker in metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-24 08:15:13 UTC
+# Last sync: 2026-05-24 09:47:35 UTC
