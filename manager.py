101d5f70a705dd8f190ceea014049e4d5d27b82d
James Holloway <james.holloway94@gmail.com>
2026-05-24 15:22:34 +0000

    resync parameter store path time for nightly audit

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-24 14:09:45 UTC
+# Last sync: 2026-05-24 15:22:34 UTC
