b0cb720e1af9173a9a9acd0e7e63b03e15279df0
James Holloway <james.holloway94@gmail.com>
2026-05-25 11:19:20 +0000

    thread cloudtrail digest sync through config banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-25 10:22:57 UTC
+# Last sync: 2026-05-25 11:19:20 UTC
