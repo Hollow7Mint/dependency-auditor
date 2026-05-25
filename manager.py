0c0f1b519d79687e27d54900722c124deda81698
James Holloway <james.holloway94@gmail.com>
2026-05-25 00:49:01 +0000

    bump known_hosts update stamp after rotation

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-24 23:56:51 UTC
+# Last sync: 2026-05-25 00:49:01 UTC
