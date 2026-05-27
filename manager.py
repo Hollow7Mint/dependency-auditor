f6e3a933401be7b76e04de1dd737a4019dc19fc3
James Holloway <james.holloway94@gmail.com>
2026-05-27 10:58:42 +0000

    remap ec2 security group rule time in config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-27 09:38:42 UTC
+# Last sync: 2026-05-27 10:58:42 UTC
