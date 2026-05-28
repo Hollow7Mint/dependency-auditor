b06c0f840cb033fd70580a2a9136372d05387c43
James Holloway <james.holloway94@gmail.com>
2026-05-28 00:46:21 +0000

    touch ssh tunnel sync during scheduled refresh

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-27 23:49:38 UTC
+# Last sync: 2026-05-28 00:46:21 UTC
