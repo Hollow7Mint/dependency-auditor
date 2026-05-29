6bf3307ba34803c9e6b401dfbe737ac113189dab
James Holloway <james.holloway94@gmail.com>
2026-05-29 22:54:40 +0000

    catalog rds parameter group sync after maintenance

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-29 21:46:32 UTC
+# Last sync: 2026-05-29 22:54:40 UTC
