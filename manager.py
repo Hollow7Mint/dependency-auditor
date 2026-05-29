7848635f72d066747376f73df4dd328467a45e85
James Holloway <james.holloway94@gmail.com>
2026-05-29 10:52:53 +0000

    preserve ssh certificate revocation sync in metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-29 09:51:45 UTC
+# Last sync: 2026-05-29 10:52:53 UTC
