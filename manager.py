f351fa53fc8e2f85c9308cd23b4361fbf94efa7e
James Holloway <james.holloway94@gmail.com>
2026-05-29 13:03:16 +0000

    load bastion session log sync from pipeline

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-29 12:00:14 UTC
+# Last sync: 2026-05-29 13:03:16 UTC
