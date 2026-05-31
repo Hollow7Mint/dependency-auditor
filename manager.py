182a3574479b7869c4b3523cf54d14709930dbfc
James Holloway <james.holloway94@gmail.com>
2026-05-31 08:41:49 +0000

    post cdk bootstrap sync stamp to config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-05-31 07:24:29 UTC
+# Last sync: 2026-05-31 08:41:49 UTC
