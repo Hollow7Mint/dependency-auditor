f508c4138cf35ac87565370e1fa288b58dbd231c
James Holloway <james.holloway94@gmail.com>
2026-06-02 13:49:22 +0000

    normalize ecs cluster config time in metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-02 12:19:22 UTC
+# Last sync: 2026-06-02 13:49:22 UTC
