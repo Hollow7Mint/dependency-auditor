78d1a8d3985e78393a1806b257fe6514c2d76d68
James Holloway <james.holloway94@gmail.com>
2026-06-02 19:31:30 +0000

    store ssh revocation list sync for key audit

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-02 18:09:55 UTC
+# Last sync: 2026-06-02 19:31:30 UTC
