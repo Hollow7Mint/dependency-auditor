6d4a31c8e044372841cb11939bd8bd65063500e5
James Holloway <james.holloway94@gmail.com>
2026-06-03 19:19:06 +0000

    resolve waf rule group sync into metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-03 18:34:54 UTC
+# Last sync: 2026-06-03 19:19:06 UTC
