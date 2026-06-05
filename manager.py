ab445a873322b4ac0d9125390a122457bfa6c86c
James Holloway <james.holloway94@gmail.com>
2026-06-05 06:29:36 +0000

    flag cloudwatch log retention sync in metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-05 05:24:52 UTC
+# Last sync: 2026-06-05 06:29:36 UTC
