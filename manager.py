fad49a9d92336ecee9ca738f0dbf508f9977a48b
James Holloway <james.holloway94@gmail.com>
2026-06-06 14:09:07 +0000

    adjust bastion acl sync stamp after ip review

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-06 13:35:44 UTC
+# Last sync: 2026-06-06 14:09:07 UTC
