ade0a52c2274d88534a3c8f87098f788e5c42279
James Holloway <james.holloway94@gmail.com>
2026-06-07 14:06:03 +0000

    document postgres index maintenance sync in record

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-07 12:36:23 UTC
+# Last sync: 2026-06-07 14:06:03 UTC
