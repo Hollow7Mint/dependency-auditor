5d7b1f74da1ff564b71f4cd15477878a3a41bfd1
James Holloway <james.holloway94@gmail.com>
2026-06-08 00:25:22 +0000

    publish ssh key distribution sync after rotation

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-07 23:45:41 UTC
+# Last sync: 2026-06-08 00:25:22 UTC
