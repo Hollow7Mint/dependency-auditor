4b87fd514d78e344529bd443b1dafa4cc5b5f166
James Holloway <james.holloway94@gmail.com>
2026-06-09 21:34:02 +0000

    write mongodb index rebuild time into metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-09 20:11:46 UTC
+# Last sync: 2026-06-09 21:34:02 UTC
