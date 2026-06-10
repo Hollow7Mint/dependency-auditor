1fb47b0d70c8df9cf7196c7469f9065f1320e48b
James Holloway <james.holloway94@gmail.com>
2026-06-10 02:12:15 +0000

    frame mysql connection limit sync in config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-10 01:17:46 UTC
+# Last sync: 2026-06-10 02:12:15 UTC
