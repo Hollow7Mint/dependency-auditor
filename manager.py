985ab4bf1a3f21852f0035b34a5394856cd75710
James Holloway <james.holloway94@gmail.com>
2026-06-14 16:15:20 +0000

    transfer redis node time to cluster banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-14 15:19:38 UTC
+# Last sync: 2026-06-14 16:15:20 UTC
