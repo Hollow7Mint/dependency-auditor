1a3f780d77e3505c06d639c5a9df9db273181612
James Holloway <james.holloway94@gmail.com>
2026-06-14 21:44:14 +0000

    set redis cluster sync stamp in config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-14 20:26:43 UTC
+# Last sync: 2026-06-14 21:44:14 UTC
