84b913e652729be3cc2c15ce555ed69108d210e2
James Holloway <james.holloway94@gmail.com>
2026-06-15 16:34:31 +0000

    relay ssh host key time through config banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-15 15:11:43 UTC
+# Last sync: 2026-06-15 16:34:31 UTC
