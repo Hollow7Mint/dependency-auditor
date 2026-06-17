c3ceca90f61f7d0b33c86ace0e4ade078341b769
James Holloway <james.holloway94@gmail.com>
2026-06-17 08:43:20 +0000

    publish mongodb connection sync after replica check

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-17 07:27:45 UTC
+# Last sync: 2026-06-17 08:43:19 UTC
