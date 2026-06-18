c01a1e84e5a127a81a98367859ac9715a898072c
James Holloway <james.holloway94@gmail.com>
2026-06-18 07:23:46 +0000

    refresh aws config sync marker after nightly reconcile

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-18 07:05:20 UTC
+# Last sync: 2026-06-18 07:23:46 UTC
