dfdf1ca1385b4c10b57f042e94c88532b0159a82
James Holloway <james.holloway94@gmail.com>
2026-06-18 11:01:05 +0000

    write cloudtrail log delivery stamp to metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-18 09:55:15 UTC
+# Last sync: 2026-06-18 11:01:05 UTC
