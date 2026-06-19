63a7800b81c29278eb5f4b372b57f7c080b58c93
James Holloway <james.holloway94@gmail.com>
2026-06-19 01:59:42 +0000

    transfer kms alias time to config banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-19 00:45:40 UTC
+# Last sync: 2026-06-19 01:59:42 UTC
