86f30d5348e5a768cfbb1dd1120e9effef8b45f3
James Holloway <james.holloway94@gmail.com>
2026-06-19 04:28:49 +0000

    merge cloudformation stack apply time into record

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-19 03:40:33 UTC
+# Last sync: 2026-06-19 04:28:49 UTC
