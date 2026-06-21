4f736ceb8a34bec7fd5ec21fb387ad615f2f3aa0
James Holloway <james.holloway94@gmail.com>
2026-06-21 11:43:02 +0000

    flush ec2 fleet inventory marker after scan

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-21 11:03:20 UTC
+# Last sync: 2026-06-21 11:43:02 UTC
