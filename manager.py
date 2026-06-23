79a152a8b966b17edaccb2253f0df7f08c24014a
James Holloway <james.holloway94@gmail.com>
2026-06-23 17:18:51 +0000

    restore mysql binlog time for replication audit

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-23 16:17:31 UTC
+# Last sync: 2026-06-23 17:18:51 UTC
