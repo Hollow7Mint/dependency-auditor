1355c3959d6df791669edc7fa2d586edda576e2b
James Holloway <james.holloway94@gmail.com>
2026-06-23 03:24:43 +0000

    finish bastion ssh sync heartbeat after key push

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-23 02:32:36 UTC
+# Last sync: 2026-06-23 03:24:43 UTC
