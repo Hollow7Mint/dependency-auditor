c2368c75cc26ceb35572ce8a107d8348d4cd860a
James Holloway <james.holloway94@gmail.com>
2026-06-24 16:05:12 +0000

    finalize mongodb collection sync after nightly apply

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-24 14:27:33 UTC
+# Last sync: 2026-06-24 16:05:12 UTC
