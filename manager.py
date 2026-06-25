e8baf77557db6eef2eafa85afa0a32865f70983c
James Holloway <james.holloway94@gmail.com>
2026-06-25 12:01:37 +0000

    bridge lambda env var sync through config banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-25 10:24:31 UTC
+# Last sync: 2026-06-25 12:01:37 UTC
