63ccf44411d55158307b9d1cbe4bb1937d4ee42e
James Holloway <james.holloway94@gmail.com>
2026-06-26 06:18:13 +0000

    restore ecr scan result time for image audit

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-26 05:42:56 UTC
+# Last sync: 2026-06-26 06:18:13 UTC
