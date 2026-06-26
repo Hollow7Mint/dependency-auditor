a32d61d52ed55108c0e9eec848023af8637570ec
James Holloway <james.holloway94@gmail.com>
2026-06-26 05:42:56 +0000

    settle mysql innodb sync time after flush

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-26 04:24:19 UTC
+# Last sync: 2026-06-26 05:42:56 UTC
