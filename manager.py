66cbbde805363600818240b25eef1e5ae30d989a
James Holloway <james.holloway94@gmail.com>
2026-06-27 17:03:28 +0000

    carry bastion config sync stamp to metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-27 16:04:18 UTC
+# Last sync: 2026-06-27 17:03:28 UTC
