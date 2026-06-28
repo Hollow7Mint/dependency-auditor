9b4f701c6497eac47a69d5fc4f3d7f24c6cbf03f
James Holloway <james.holloway94@gmail.com>
2026-06-28 16:48:44 +0000

    relay db connection string time through config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-28 15:54:42 UTC
+# Last sync: 2026-06-28 16:48:43 UTC
