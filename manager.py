5a08257cc8135b225f0a9f64e2aef2ebce1787c2
James Holloway <james.holloway94@gmail.com>
2026-06-28 00:14:46 +0000

    seal inspector finding sync in security banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-27 23:20:19 UTC
+# Last sync: 2026-06-28 00:14:46 UTC
