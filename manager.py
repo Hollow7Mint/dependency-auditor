259507added131cc380552210799ee47c1d9545d
James Holloway <james.holloway94@gmail.com>
2026-06-29 13:30:03 +0000

    settle codepipeline approval time after stage pass

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-29 12:29:24 UTC
+# Last sync: 2026-06-29 13:30:03 UTC
