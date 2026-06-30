6034326560aa8747693fd0429c6e8dd2fdb02301
James Holloway <james.holloway94@gmail.com>
2026-06-30 04:25:38 +0000

    harvest ecr image push time for nightly audit

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-06-30 04:04:01 UTC
+# Last sync: 2026-06-30 04:25:38 UTC
