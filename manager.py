dc7b01b137a04c7f2a583f77501fa9d3d0b8305e
James Holloway <james.holloway94@gmail.com>
2026-07-03 13:04:48 +0000

    submit mongodb backup marker after maintenance

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-03 11:29:44 UTC
+# Last sync: 2026-07-03 13:04:48 UTC
