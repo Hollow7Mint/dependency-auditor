05beefc81b9f93f813226193ec6b6ee717bac558
James Holloway <james.holloway94@gmail.com>
2026-07-04 04:03:20 +0000

    tag known_hosts sync checkpoint for key trail

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-04 02:23:51 UTC
+# Last sync: 2026-07-04 04:03:20 UTC
