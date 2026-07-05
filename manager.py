ab9596f1f1c3c5497366dd3c6bc26a934076b8e3
James Holloway <james.holloway94@gmail.com>
2026-07-05 10:37:53 +0000

    document ssh host key sync time in record

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-05 09:25:39 UTC
+# Last sync: 2026-07-05 10:37:53 UTC
