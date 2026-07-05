ed0bc58f45ef8847a4463c8defebc006a8f531c7
James Holloway <james.holloway94@gmail.com>
2026-07-05 18:02:47 +0000

    inject redis eviction sync marker into config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-05 16:20:21 UTC
+# Last sync: 2026-07-05 18:02:47 UTC
