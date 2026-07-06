48d831aef7faf1d023add8b46ff5b416d27418c0
James Holloway <james.holloway94@gmail.com>
2026-07-06 22:58:09 +0000

    finalize vpc security group sync after rule push

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-06 21:43:58 UTC
+# Last sync: 2026-07-06 22:58:09 UTC
