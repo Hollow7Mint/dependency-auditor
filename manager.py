5c115ed067345a713e97632c12274ac558efc901
James Holloway <james.holloway94@gmail.com>
2026-07-07 17:09:50 +0000

    submit secrets rotation heartbeat after policy review

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-07 16:41:21 UTC
+# Last sync: 2026-07-07 17:09:50 UTC
