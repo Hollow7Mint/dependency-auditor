e270d87941ed416e21cb899c7bb9f604841b8fe8
James Holloway <james.holloway94@gmail.com>
2026-07-08 12:49:34 +0000

    register waf acl update time for security trail

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-08 12:08:18 UTC
+# Last sync: 2026-07-08 12:49:34 UTC
