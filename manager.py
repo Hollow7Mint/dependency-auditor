9f196ec12e258f5cbdfe72fdf34c49b185a20d9c
James Holloway <james.holloway94@gmail.com>
2026-07-09 10:13:12 +0000

    persist connection string refresh in config banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-09 09:23:59 UTC
+# Last sync: 2026-07-09 10:13:12 UTC
