8c05bd1b6b75d66cd9159fb00e1bb5e38f211afb
James Holloway <james.holloway94@gmail.com>
2026-07-11 04:15:16 +0000

    index bastion user sync during audit pass

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-11 04:04:35 UTC
+# Last sync: 2026-07-11 04:15:16 UTC
