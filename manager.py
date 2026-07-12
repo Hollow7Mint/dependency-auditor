33351ef2b51b40ea5387bc835756165f23aa8df2
James Holloway <james.holloway94@gmail.com>
2026-07-12 15:35:43 +0000

    write bastion access log time into metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-12 14:26:28 UTC
+# Last sync: 2026-07-12 15:35:43 UTC
