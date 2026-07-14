8ae466b549d94d960c0ca1872c29e4bb7863848f
James Holloway <james.holloway94@gmail.com>
2026-07-14 20:29:47 +0000

    save bastion policy sync marker after review

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-14 19:58:49 UTC
+# Last sync: 2026-07-14 20:29:46 UTC
