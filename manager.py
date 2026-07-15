bd4e9901cb588aa26be0046221aff2070a3099c6
James Holloway <james.holloway94@gmail.com>
2026-07-15 23:38:22 +0000

    advance cdk app deploy time into config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-15 22:39:47 UTC
+# Last sync: 2026-07-15 23:38:22 UTC
