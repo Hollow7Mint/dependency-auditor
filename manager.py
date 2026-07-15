e17f085cb55a6b5989f206cee2616ac982a06c6b
James Holloway <james.holloway94@gmail.com>
2026-07-15 04:16:18 +0000

    settle ssh known algorithm sync time after update

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-15 03:13:08 UTC
+# Last sync: 2026-07-15 04:16:18 UTC
