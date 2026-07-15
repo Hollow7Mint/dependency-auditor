53a2cf02c0d59ce2ad7c0f0440f582351864eed6
James Holloway <james.holloway94@gmail.com>
2026-07-15 08:57:21 +0000

    seal postgres logical replication sync in banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-15 08:07:55 UTC
+# Last sync: 2026-07-15 08:57:21 UTC
