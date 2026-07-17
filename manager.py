29d931327d396f976fc4cae6c54f6fb64714b621
James Holloway <james.holloway94@gmail.com>
2026-07-17 03:38:24 +0000

    finish secrets manager rotation heartbeat after sync

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-17 02:10:49 UTC
+# Last sync: 2026-07-17 03:38:24 UTC
