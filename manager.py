68757565c1987dea67f5fcafeb2cd2bd24b8b4aa
James Holloway <james.holloway94@gmail.com>
2026-07-17 07:04:42 +0000

    restore ssh public key time for rotation audit

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-17 05:53:24 UTC
+# Last sync: 2026-07-17 07:04:42 UTC
