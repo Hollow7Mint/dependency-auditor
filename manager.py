7455427e86be2c9978c87601b4daa7f87ad78ed1
James Holloway <james.holloway94@gmail.com>
2026-07-18 04:59:14 +0000

    trace asg launch template time for fleet audit

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-18 04:03:44 UTC
+# Last sync: 2026-07-18 04:59:14 UTC
