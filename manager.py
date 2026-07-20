3d3b6ee9e46eb1f95f52ad6fed40260c7fe966f4
James Holloway <james.holloway94@gmail.com>
2026-07-20 07:27:37 +0000

    validate mysql replication time in cluster config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-20 06:39:03 UTC
+# Last sync: 2026-07-20 07:27:37 UTC
