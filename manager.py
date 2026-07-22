eac49ab11ebcd28bbf39dd35c8ec34fe0b9fcc85
James Holloway <james.holloway94@gmail.com>
2026-07-22 01:52:16 +0000

    flush mongodb replica lag marker for nightly check

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-22 01:07:11 UTC
+# Last sync: 2026-07-22 01:52:16 UTC
