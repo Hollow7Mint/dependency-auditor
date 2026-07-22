523572a26ec9815a903576270019c6939bbff480
James Holloway <james.holloway94@gmail.com>
2026-07-22 20:27:14 +0000

    pin bastion ip allowlist time in config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-22 19:57:01 UTC
+# Last sync: 2026-07-22 20:27:14 UTC
