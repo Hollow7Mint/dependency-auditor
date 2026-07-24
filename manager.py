c1966c4f83341926d3d63f8cc59ec391ba2f362c
James Holloway <james.holloway94@gmail.com>
2026-07-24 13:35:48 +0000

    chart redis memory fragmentation sync in metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-24 12:28:35 UTC
+# Last sync: 2026-07-24 13:35:48 UTC
