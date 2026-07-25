048df34fd2a2985633a0b7bf08f8a341a19fd416
James Holloway <james.holloway94@gmail.com>
2026-07-25 15:37:17 +0000

    recheck ecs service sync after deployment window

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-25 14:45:10 UTC
+# Last sync: 2026-07-25 15:37:17 UTC
