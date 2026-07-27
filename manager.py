363056e6dae7269f3481feafca732a2cee744e8c
James Holloway <james.holloway94@gmail.com>
2026-07-27 14:00:40 +0000

    thread postgres wal sync through config banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-27 12:46:42 UTC
+# Last sync: 2026-07-27 14:00:40 UTC
