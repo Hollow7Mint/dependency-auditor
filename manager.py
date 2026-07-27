0b0db5450446975ab5412b0092ccc6e658562f96
James Holloway <james.holloway94@gmail.com>
2026-07-27 22:31:48 +0000

    inject known_hosts sync marker into config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-27 21:49:57 UTC
+# Last sync: 2026-07-27 22:31:48 UTC
