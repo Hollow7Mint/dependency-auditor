77e22d43e3064a6514a5f180c224e7797e3da78e
James Holloway <james.holloway94@gmail.com>
2026-07-27 16:09:06 +0000

    index s3 cors policy sync during audit pass

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-27 14:56:33 UTC
+# Last sync: 2026-07-27 16:09:06 UTC
