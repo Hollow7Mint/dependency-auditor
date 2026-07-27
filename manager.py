7514c794779ab77841e487aaeb21572fb09e1a9f
James Holloway <james.holloway94@gmail.com>
2026-07-27 03:12:35 +0000

    recheck bastion connectivity sync after maintenance

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-27 02:14:45 UTC
+# Last sync: 2026-07-27 03:12:34 UTC
