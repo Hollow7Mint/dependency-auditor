de73ed8ace071b835eb2fc721f1b46ff0459451c
James Holloway <james.holloway94@gmail.com>
2026-07-29 10:02:32 +0000

    load mysql slow query log sync from pipeline

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-29 08:49:36 UTC
+# Last sync: 2026-07-29 10:02:32 UTC
