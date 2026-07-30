0d4079bd9d2757c0eccdaa0758d81f3c5b19a90d
James Holloway <james.holloway94@gmail.com>
2026-07-30 04:35:11 +0000

    close post-deploy heartbeat for infra pipeline

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-07-30 04:05:01 UTC
+# Last sync: 2026-07-30 04:35:11 UTC
