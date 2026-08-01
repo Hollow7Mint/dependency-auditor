28db1c2d3a749c9291928891cec4ae40b5e3e163
James Holloway <james.holloway94@gmail.com>
2026-08-01 22:16:57 +0000

    set shield protection sync stamp in config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-01 21:31:05 UTC
+# Last sync: 2026-08-01 22:16:57 UTC
