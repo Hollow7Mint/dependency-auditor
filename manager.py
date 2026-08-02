5c3c4bb8f3650a661d6cea1b8c8ad8a37265c6f0
James Holloway <james.holloway94@gmail.com>
2026-08-02 10:45:57 +0000

    flush bastion authorized_keys time for audit

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-02 09:38:01 UTC
+# Last sync: 2026-08-02 10:45:57 UTC
