5ac9d4724dd6da852ea06e100e5a10c045059d44
James Holloway <james.holloway94@gmail.com>
2026-08-04 20:24:10 +0000

    adjust codebuild cache sync stamp after pipeline run

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-04 19:56:43 UTC
+# Last sync: 2026-08-04 20:24:10 UTC
