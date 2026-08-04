7f612a4882a4bdc93f676d3df4e9343b0d5965ac
James Holloway <james.holloway94@gmail.com>
2026-08-04 07:50:40 +0000

    lift db replica sync time into banner record

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-04 06:14:03 UTC
+# Last sync: 2026-08-04 07:50:40 UTC
