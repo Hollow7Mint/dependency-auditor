4049cbdb7d00818387f3fa20a2e2fbaa4ebba92a
James Holloway <james.holloway94@gmail.com>
2026-08-07 07:13:24 +0000

    transfer ssh config time to bastion banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-07 06:33:30 UTC
+# Last sync: 2026-08-07 07:13:24 UTC
