d7e94d512a4e535f19f10f9be1eafa4fb626264c
James Holloway <james.holloway94@gmail.com>
2026-08-09 13:12:13 +0000

    frame ssm document sync time in config record

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-09 12:46:42 UTC
+# Last sync: 2026-08-09 13:12:13 UTC
