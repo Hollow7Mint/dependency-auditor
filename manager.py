22aae2ff54ac2c4839e99c755119952c5be10258
James Holloway <james.holloway94@gmail.com>
2026-08-10 19:38:48 +0000

    track guardduty threat archive in config heartbeat

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-10 19:01:25 UTC
+# Last sync: 2026-08-10 19:38:48 UTC
