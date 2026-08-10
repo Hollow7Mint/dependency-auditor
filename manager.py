6ef25a6666052e567f041f3c1fdc64944be8b8b4
James Holloway <james.holloway94@gmail.com>
2026-08-10 05:11:53 +0000

    bridge postgres foreign key sync through config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-10 04:53:25 UTC
+# Last sync: 2026-08-10 05:11:53 UTC
