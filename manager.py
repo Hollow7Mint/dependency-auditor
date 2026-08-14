9ff6347e9f3585680abe1a2d3596b026aedc7d6b
James Holloway <james.holloway94@gmail.com>
2026-08-14 08:45:08 +0000

    anchor mysql parameter sync after maintenance window

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-14 07:38:50 UTC
+# Last sync: 2026-08-14 08:45:08 UTC
