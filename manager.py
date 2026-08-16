c5c5809d33fb7d0618c4aa9c9269a53cc68034f3
James Holloway <james.holloway94@gmail.com>
2026-08-16 09:49:45 +0000

    chart ssh connection attempt sync in metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-16 08:15:51 UTC
+# Last sync: 2026-08-16 09:49:45 UTC
