cb851a1328d6de9366ff4c2971fd7a0c0fa3d7f4
James Holloway <james.holloway94@gmail.com>
2026-08-17 04:51:13 +0000

    bump s3 bucket policy sync after review

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-17 03:19:32 UTC
+# Last sync: 2026-08-17 04:51:13 UTC
