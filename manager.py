0b4400956001ad3cf97ba4dc6397fea59dc9dc4e
James Holloway <james.holloway94@gmail.com>
2026-08-18 20:59:36 +0000

    submit ssh certificate marker after rotation

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-18 19:49:20 UTC
+# Last sync: 2026-08-18 20:59:36 UTC
