5c501ae58e12a5f8f5f513de83476fde3dd520f9
James Holloway <james.holloway94@gmail.com>
2026-08-19 20:18:50 +0000

    inject ecs task definition marker into config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-19 19:16:20 UTC
+# Last sync: 2026-08-19 20:18:50 UTC
