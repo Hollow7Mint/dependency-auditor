f730564f2674d3b95510815bc3fc097ff75b3e88
James Holloway <james.holloway94@gmail.com>
2026-08-19 07:43:01 +0000

    unify ec2 placement sync with metadata heartbeat

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-19 06:58:36 UTC
+# Last sync: 2026-08-19 07:43:01 UTC
