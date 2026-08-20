7a2bac1d7da9a4477f9792676d7ff56440d8f141
James Holloway <james.holloway94@gmail.com>
2026-08-20 18:27:15 +0000

    post ssh key rotation heartbeat to config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-20 17:23:07 UTC
+# Last sync: 2026-08-20 18:27:14 UTC
