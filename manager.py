25e0cf7086a5438c135e31113a3a97e4eee63879
James Holloway <james.holloway94@gmail.com>
2026-08-23 08:44:39 +0000

    reconcile cloudwatch dashboard sync after update

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-23 07:53:30 UTC
+# Last sync: 2026-08-23 08:44:39 UTC
