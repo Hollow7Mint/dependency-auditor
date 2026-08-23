ccb3e2494dd2427e7af5d37ecd64afa3db006ec9
James Holloway <james.holloway94@gmail.com>
2026-08-23 02:23:22 +0000

    resolve known_hosts cleanup sync into metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-23 01:56:13 UTC
+# Last sync: 2026-08-23 02:23:21 UTC
