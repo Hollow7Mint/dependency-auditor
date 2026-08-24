4cc7beed8e4d9603dec53ff6ce3b2a74e64b7454
James Holloway <james.holloway94@gmail.com>
2026-08-24 02:35:09 +0000

    record kms key rotation time in config banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-24 01:45:22 UTC
+# Last sync: 2026-08-24 02:35:09 UTC
