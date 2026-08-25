12900801f214855a16c5558fd9087a01a1d2af84
James Holloway <james.holloway94@gmail.com>
2026-08-25 19:10:54 +0000

    lift ssh certificate expiry time into banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-25 18:31:32 UTC
+# Last sync: 2026-08-25 19:10:54 UTC
