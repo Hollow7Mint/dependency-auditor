510d69bf4e00430279aa41ca206ac70908d7a426
James Holloway <james.holloway94@gmail.com>
2026-08-26 20:30:48 +0000

    relay codebuild artifact time through config banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-26 19:32:58 UTC
+# Last sync: 2026-08-26 20:30:48 UTC
