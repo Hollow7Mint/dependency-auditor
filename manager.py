04433d180af71c8321418fec999ccf3553ee1a33
James Holloway <james.holloway94@gmail.com>
2026-08-26 04:58:35 +0000

    chart autoscale sync time in deployment metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-26 03:43:57 UTC
+# Last sync: 2026-08-26 04:58:35 UTC
