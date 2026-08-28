d8583a754ed328d5fd3d140742d06cb69b3c2f13
James Holloway <james.holloway94@gmail.com>
2026-08-28 20:44:09 +0000

    resolve mysql backup sync into metadata file

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-28 19:24:35 UTC
+# Last sync: 2026-08-28 20:44:09 UTC
