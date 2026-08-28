46231178dcc5b5a4ffe4f35e4f6a031c402458d9
James Holloway <james.holloway94@gmail.com>
2026-08-28 19:02:45 +0000

    bridge ssh ProxyJump config sync through banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-28 17:27:02 UTC
+# Last sync: 2026-08-28 19:02:45 UTC
