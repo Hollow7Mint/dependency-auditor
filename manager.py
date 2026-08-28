a63d3ac8c2e4a50ca9a5a325d623c8bb77999664
James Holloway <james.holloway94@gmail.com>
2026-08-28 13:51:39 +0000

    queue cloudfront distribution stamp after invalidation

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-28 12:11:18 UTC
+# Last sync: 2026-08-28 13:51:39 UTC
