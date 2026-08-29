8e72f732ebf37e3cefea868146c2cbb3559c4200
James Holloway <james.holloway94@gmail.com>
2026-08-29 15:53:31 +0000

    preserve mysql user permission sync in metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-29 15:01:09 UTC
+# Last sync: 2026-08-29 15:53:31 UTC
