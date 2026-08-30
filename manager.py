452f6e3275be3c64d57f52a12f8420bf28dde9e4
James Holloway <james.holloway94@gmail.com>
2026-08-30 14:46:02 +0000

    recheck mongodb write concern sync after update

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-30 14:05:38 UTC
+# Last sync: 2026-08-30 14:46:02 UTC
