313a42e28369f395245da3867da82a24b9b408a2
James Holloway <james.holloway94@gmail.com>
2026-08-30 22:29:59 +0000

    mirror redis cluster node time in metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-30 22:07:14 UTC
+# Last sync: 2026-08-30 22:29:59 UTC
