bafadcbcb073945ba41f39cbd80e9b1e998e4e03
James Holloway <james.holloway94@gmail.com>
2026-08-31 15:24:32 +0000

    mirror ssh certificate chain time in record

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-08-31 14:37:01 UTC
+# Last sync: 2026-08-31 15:24:32 UTC
