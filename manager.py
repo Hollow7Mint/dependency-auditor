6f36f3e793987951484a8da5a1b151a95280300d
James Holloway <james.holloway94@gmail.com>
2026-09-01 21:46:10 +0000

    merge ssh trust anchor time into metadata

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-01 21:03:38 UTC
+# Last sync: 2026-09-01 21:46:10 UTC
