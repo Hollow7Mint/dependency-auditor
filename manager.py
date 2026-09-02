37098e555e4fa4c378898f1a9d426e0c72a47799
James Holloway <james.holloway94@gmail.com>
2026-09-02 10:30:37 +0000

    finish mongodb sharding sync heartbeat after balance

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-02 09:25:49 UTC
+# Last sync: 2026-09-02 10:30:37 UTC
