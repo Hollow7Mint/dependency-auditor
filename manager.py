fd636606e2f7a48b559e3fca3f553994ed6d0c3c
James Holloway <james.holloway94@gmail.com>
2026-09-02 11:55:48 +0000

    anchor cloudwatch alarm threshold time in config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-02 10:30:37 UTC
+# Last sync: 2026-09-02 11:55:48 UTC
