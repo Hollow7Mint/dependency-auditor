c154ac24afae722ec1ae0d234b9b2d3a43da439e
James Holloway <james.holloway94@gmail.com>
2026-09-02 06:40:30 +0000

    annotate ec2 ami bake time for deploy record

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-02 06:05:28 UTC
+# Last sync: 2026-09-02 06:40:30 UTC
