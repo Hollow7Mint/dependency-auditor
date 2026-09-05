14739bd7325ef0d85de9ce1887e96eb1956a5e0f
James Holloway <james.holloway94@gmail.com>
2026-09-05 18:38:57 +0000

    tag postgres checkpoint sync for replication trail

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-05 17:18:15 UTC
+# Last sync: 2026-09-05 18:38:56 UTC
