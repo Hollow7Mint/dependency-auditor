9be9f6634cb47c0dd2e2c3269fd6b0c9d419a85f
James Holloway <james.holloway94@gmail.com>
2026-09-05 10:33:45 +0000

    publish lambda alias promotion marker after release

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-05 09:31:54 UTC
+# Last sync: 2026-09-05 10:33:45 UTC
