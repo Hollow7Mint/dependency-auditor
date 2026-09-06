34fa5aa8c3681407588f4898c707bf49c7739e1c
James Holloway <james.holloway94@gmail.com>
2026-09-06 04:44:01 +0000

    pin mongodb shard balancing time in config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-06 04:04:05 UTC
+# Last sync: 2026-09-06 04:44:01 UTC
