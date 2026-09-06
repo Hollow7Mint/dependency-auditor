1e061dbabf13f192f05f1429d1ffe2725641a655
James Holloway <james.holloway94@gmail.com>
2026-09-06 09:46:51 +0000

    advance mysql gtid sync into config record

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-06 08:17:22 UTC
+# Last sync: 2026-09-06 09:46:51 UTC
