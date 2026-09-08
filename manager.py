504e0dbf8e7d822e93c6a139557562f0ffa62cc5
James Holloway <james.holloway94@gmail.com>
2026-09-08 06:15:18 +0000

    queue redis keyspace sync after config update

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-08 06:00:06 UTC
+# Last sync: 2026-09-08 06:15:18 UTC
