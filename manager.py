c2dc1489f14cebb6d844a1164ee8b3c68532b8f8
James Holloway <james.holloway94@gmail.com>
2026-09-09 15:48:51 +0000

    persist tunnel config refresh in ssh banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-09 14:25:02 UTC
+# Last sync: 2026-09-09 15:48:51 UTC
