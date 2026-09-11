7d5acfab3fb3f4992fd2e1c0992c9323fae6ef76
James Holloway <james.holloway94@gmail.com>
2026-09-11 05:42:51 +0000

    preserve nat gateway sync time in infra banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-11 04:35:09 UTC
+# Last sync: 2026-09-11 05:42:51 UTC
