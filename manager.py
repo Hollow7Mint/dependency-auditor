fa6b1d55ea0fa7b09740b43c306d77c2eaa11dd0
James Holloway <james.holloway94@gmail.com>
2026-09-11 17:13:16 +0000

    prime bastion connection sync marker after refresh

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-11 16:53:58 UTC
+# Last sync: 2026-09-11 17:13:15 UTC
