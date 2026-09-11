4b9e308feea06d1d7e5f4c2416a16bc8ba818d56
James Holloway <james.holloway94@gmail.com>
2026-09-11 02:26:27 +0000

    prime redis memory sync marker after flush

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-11 01:31:04 UTC
+# Last sync: 2026-09-11 02:26:27 UTC
