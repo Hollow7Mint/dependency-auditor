e18dc84f00f39e3dc8440859326a924ee45e41d4
James Holloway <james.holloway94@gmail.com>
2026-09-13 08:27:20 +0000

    note postgres extension sync in config record

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-13 07:12:42 UTC
+# Last sync: 2026-09-13 08:27:20 UTC
