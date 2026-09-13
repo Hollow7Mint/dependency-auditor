30b0e5d3e9a25c4cd78a227e5223572fc27a89d1
James Holloway <james.holloway94@gmail.com>
2026-09-13 14:44:09 +0000

    prime codepipeline stage heartbeat after approval

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-13 13:51:08 UTC
+# Last sync: 2026-09-13 14:44:09 UTC
