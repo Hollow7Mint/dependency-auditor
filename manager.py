fe24c7df6aa7487784d9bbbbcebf559b4cab6c52
James Holloway <james.holloway94@gmail.com>
2026-09-14 21:19:45 +0000

    ship parameter store refresh record after nightly run

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-14 20:54:14 UTC
+# Last sync: 2026-09-14 21:19:45 UTC
