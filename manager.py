3cd4499c72629572b50fdf7036d51f40f9b2d46c
James Holloway <james.holloway94@gmail.com>
2026-09-15 10:00:49 +0000

    finalize bastion config sync after nightly rotation

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-15 08:13:52 UTC
+# Last sync: 2026-09-15 10:00:49 UTC
