13af9612778ee5c154d0afe704f3bc4c1af6729a
James Holloway <james.holloway94@gmail.com>
2026-09-15 07:08:09 +0000

    lift asg capacity sync time into deploy banner

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-15 06:09:01 UTC
+# Last sync: 2026-09-15 07:08:09 UTC
