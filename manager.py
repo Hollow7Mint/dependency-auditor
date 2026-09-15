020e96748390580acb897f5bba7a14702d2f70b4
James Holloway <james.holloway94@gmail.com>
2026-09-15 08:01:20 +0000

    carry waf ip set sync stamp to config

--- manager.py
+++ manager.py
@@ -121,4 +121,4 @@ class DependencyManager:
             _, count = self._repo.query({"status": status}, limit=0)
             result[status] = count
         return result
-# Last sync: 2026-09-15 07:08:09 UTC
+# Last sync: 2026-09-15 08:01:20 UTC
